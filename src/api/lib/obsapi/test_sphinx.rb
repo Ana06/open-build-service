228c5306f75ff7f26779cc21da26ff64d5c2acb1
Eduardo Navarro <enavarro@suse.com>
2019-07-04 18:08:28 +0200

    Remove unnecessary start command for sphinx
    With the update of the thinking-sphinx gem, the command 'start' is not
    recognized any more. We remove it from the ministests initialization, as
    it seems it is also not needed.
    
    Co-authored-by: Saray Cabrera Padrón <scabrerapadron@suse.de>

--- src/api/lib/obsapi/test_sphinx.rb
+++ src/api/lib/obsapi/test_sphinx.rb
@@ -11,11 +11,10 @@ module OBSApi
         Rails.logger.debug 'Indexing and starting Sphinx'
         # Ensure sphinx directories exist for the test environment
         ThinkingSphinx::Test.init
-        ThinkingSphinx::Test.start
-        # Index
-        ThinkingSphinx::Test.index
         # Configure and start Sphinx
         ThinkingSphinx::Test.start_with_autostop
+        # Index
+        ThinkingSphinx::Test.index
         Rails.logger.debug 'Sphinx is up and running'
         @started = true
       end
